a806ec8765310eee295925ce0a23d48abeb96da6
kozzy1 <kozzy1@gmail.com>
2017-07-17 11:38:32 +1000

    add unzip for win

--- toolkit/DownloadDatasetSintel.m
+++ toolkit/DownloadDatasetSintel.m
@@ -1,12 +1,14 @@
 function DownloadDatasetSintel()
 command_wget = 'wget';
+command_unzip = 'unzip';
 if ispc
     command_wget = '"toolkit_win/wget"';
+    command_unzip = '"toolkit_win/unzip"';
 end
 
 system([command_wget ' -c http://files.is.tue.mpg.de/jwulff/sintel/MPI-Sintel-stereo-training-20150305.zip']);
 mkdir DatasetSintel;
 cd DatasetSintel;
-unzip ../MPI-Sintel-stereo-training-20150305.zip;
+system([command_unzip '../MPI-Sintel-stereo-training-20150305.zip']);
 cd ..
 end
